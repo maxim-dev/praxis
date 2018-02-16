d9b2970e7e4d5b995e944a70cdbb6149c6884a5a
Anna Malutina <anna.malutina@bizmosis.com>
2018-02-16 16:42:27 +0300

    был добавлен список

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,7 +8,8 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: string[];
+  //items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -18,7 +19,8 @@ export class ListPage {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
+    this.items = ['person', 'school', 'red', 'boat'];
+    /*
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
@@ -26,6 +28,7 @@ export class ListPage {
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+    */
   }
 
   itemTapped(event, item) {
